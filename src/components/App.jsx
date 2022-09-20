3063ef125cfe50b96a32da6d420cc5ffa8eb953e
SeRgNeschetniy <xdsergx@gmail.com>
2022-09-20 16:17:04 +0300

    add app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ImageGallery from './ImageGallery/ImageGallery';
+import { Searchbar } from './Searchbar/Searchbar';
+import * as API from '../API/Api';
+import Button from './Button/Button';
+import Modal from './Modal/Modal';
+import { Container } from './App.module';
+export default class App extends Component {
+  state = {
+    page: 1,
+    query: '',
+    items: [],
+    largeImageURL: '',
+  };
+
+  addImages = async (query, page) => {
+    const images = await API.fetchImages(query, page);
+    this.setState(prevState => ({
+      items: [...prevState.items, ...images],
+    }));
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    if (prevState.query !== query || prevState.page !== page) {
+      this.addImages(query, page);
+    }
+  }
+
+  handleSearchSubmit = query => {
+    this.setState({
+      query,
+      items: [],
+      page: 1,
+    });
+  };
+
+  onLoadMore = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  onOpenModal = largeImageURL => {
+    this.setState({ largeImageURL });
+  };
+
+  onCloseModal = () => {
+    this.setState({ largeImageURL: '' });
+  };
+
+  render() {
+    const { items, largeImageURL } = this.state;
+    return (
+      <Container>
+        <Searchbar onSearch={this.handleSearchSubmit} />
+        {items.length > 0 && (
+          <ImageGallery items={items} onClick={this.onOpenModal} />
+        )}
+        {items.length > 0 && <Button onLoadMore={this.onLoadMore} />}
+        {largeImageURL && (
+          <Modal onClose={this.onCloseModal} largeImageURL={largeImageURL} />
+        )}
+      </Container>
+    );
+  }
+}
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,17 @@
+import { MagnifyingGlass } from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+
+export default function Loader() {
+  return (
+    <MagnifyingGlass
+      visible={true}
+      height="80"
+      width="80"
+      ariaLabel="MagnifyingGlass-loading"
+      wrapperStyle={{}}
+      wrapperClass="MagnifyingGlass-wrapper"
+      glassColor="#c0efff"
+      color="#e15b64"
+    />
+  );
+}
